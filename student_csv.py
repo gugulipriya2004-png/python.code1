6c9fcaf7feff495e713077bac00fd51df544d3fd
gugulipriya2004-png <guguli.priya2004@gmail.com>
2025-08-31 15:05:24 +0530

    Add files via upload

---
+++ student_csv.py
@@ -0,0 +1,27 @@
+s,t,u,d,e,n,t,:,m,i,k,i,",",r,o,l,l,:,1,8,5,",",m,a,r,k,:,8,9,",",b,r,a,n,c,h,:,d,a,t,a,s,c,i,e,n,c,e
+a,r,k,:, ,9,4, ,",",b,r,a,n,c,h,:,c,s,e
+s,t,u,d,e,n,t,:,j,i,y,a, ,",",r,o,l,l,:,1,7,3, ,",",m,a,r,k,:, ,3,2, ,",",b,r,a,n,c,h,:,m,c,a
+s,t,u,d,e,n,t,:,s,i,y,a, ,",",r,o,l,l,:,1,7,7, ,",",m,a,r,k,:, ,7,2, ,",",b,r,a,n,c,h,:,m,-,t,e,c,h
+s,t,u,d,e,n,t,:,k,i,y,a, ,",",r,o,l,l,:,1,8,3, ,",",m,a,r,k,:, ,9,8, ,",",b,r,a,n,c,h,:,a,i,m,l
+s,t,u,d,e,n,t,:,m,i,y,a, ,",",r,o,l,l,:,1,7,3, ,",",m,a,r,k,:, ,3,2, ,",",b,r,a,n,c,h,:,c,s,e
+s,t,u,d,e,n,t,:,f,i,y,a, ,",",r,o,l,l,:,1,7,3, ,",",m,a,r,k,:, ,3,2, ,",",b,r,a,n,c,h,:,m,-,t,e,c,h
+s,t,u,d,e,n,t,:,g,i,y,a, ,",",r,o,l,l,:,1,7,3, ,",",m,a,r,k,:, ,3,2, ,",",b,r,a,n,c,h,:,m,c,a
+s,t,u,d,e,n,t,:,h,i,y,a, ,",",r,o,l,l,:,1,7,3, ,",",m,a,r,k,:, ,3,2, ,",",b,r,a,n,c,h,:,a,i,m,l
+s,t,u,d,e,n,t,:,c,i,y,a, ,",",r,o,l,l,:,1,7,3, ,",",m,a,r,k,:, ,3,2, ,",",b,r,a,n,c,h,:,c,s,e
+s,t,u,d,e,n,t,:,j,i,y,a, ,",",r,o,l,l,:,1,7,3, ,",",m,a,r,k,:, ,3,2, ,",",b,r,a,n,c,h,:,m,-,t,e,c,h
+s,t,u,d,e,n,t,:,k,i,y,a, ,",",r,o,l,l,:,1,7,3, ,",",m,a,r,k,:, ,3,2, ,",",b,r,a,n,c,h,:,a,i,m,l
+s,t,u,d,e,n,t,:,l,i,y,a, ,",",r,o,l,l,:,1,7,3, ,",",m,a,r,k,:, ,3,2, ,",",b,r,a,n,c,h,:,m,c,a
+s,t,u,d,e,n,t,:,m,i,y,a, ,",",r,o,l,l,:,1,7,3, ,",",m,a,r,k,:, ,3,2, ,",",b,r,a,n,c,h,:,c,s,e
+s,t,u,d,e,n,t,:,n,i,y,a, ,",",r,o,l,l,:,1,7,3, ,",",m,a,r,k,:, ,3,2, ,",",b,r,a,n,c,h,:,m,c,a
+s,t,u,d,e,n,t,:,o,i,y,a, ,",",r,o,l,l,:,1,7,3, ,",",m,a,r,k,:, ,3,2, ,",",b,r,a,n,c,h,:,m,-,t,e,c,h
+s,t,u,d,e,n,t,:,p,i,y,a, ,",",r,o,l,l,:,1,7,3, ,",",m,a,r,k,:, ,3,2, ,",",b,r,a,n,c,h,:,m,c,a
+s,t,u,d,e,n,t,:,q,i,y,a, ,",",r,o,l,l,:,1,7,3, ,",",m,a,r,k,:, ,3,2, ,",",b,r,a,n,c,h,:,c,s,e
+s,t,u,d,e,n,t,:,r,i,y,a, ,",",r,o,l,l,:,1,7,3, ,",",m,a,r,k,:, ,3,2, ,",",b,r,a,n,c,h,:,a,i,m,l
+s,t,u,d,e,n,t,:,t,i,y,a, ,",",r,o,l,l,:,1,7,3, ,",",m,a,r,k,:, ,3,2, ,",",b,r,a,n,c,h,:,m,-,t,e,c,h
+s,t,u,d,e,n,t,:,s,i,y,a, ,",",r,o,l,l,:,1,7,3, ,",",m,a,r,k,:, ,3,2, ,",",b,r,a,n,c,h,:,m,c,a
+s,t,u,d,e,n,t,:,u,i,y,a, ,",",r,o,l,l,:,1,7,3, ,",",m,a,r,k,:, ,3,2, ,",",b,r,a,n,c,h,:,c,s,e
+s,t,u,d,e,n,t,:,i,v,y,a, ,",",r,o,l,l,:,1,7,3, ,",",m,a,r,k,:, ,3,2, ,",",b,r,a,n,c,h,:,a,i,m,l
+s,t,u,d,e,n,t,:,w,i,y,a, ,",",r,o,l,l,:,1,7,3, ,",",m,a,r,k,:, ,3,2, ,",",b,r,a,n,c,h,:,m,-,t,e,c,h
+s,t,u,d,e,n,t,:,x,i,y,a, ,",",r,o,l,l,:,1,7,3, ,",",m,a,r,k,:, ,3,2, ,",",b,r,a,n,c,h,:,m,c,a
+s,t,u,d,e,n,t,:,y,i,y,a, ,",",r,o,l,l,:,1,7,3, ,",",m,a,r,k,:, ,3,2, ,",",b,r,a,n,c,h,:,c,s,e
+s,t,u,d,e,n,t,:,y,i,y,a, ,",",r,o,l,l,:,1,7,3, ,",",m,a,r,k,:, ,3,2, ,",",b,r,a,n,c,h,:,c,s,e
